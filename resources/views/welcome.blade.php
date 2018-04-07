e9bae1a0c67d7172407ed2a5f6b2c0210601f364
Manolo Marin <manolinsk8@hotmail.com>
2018-04-07 15:33:38 -0500

    modificacion index y solucion de error migraciones iniciales

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,15 +16,14 @@
     
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
     .row.content {height: 450px}
-    
+
     /* Set gray background color and 100% height */
     .sidenav {
       padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
-    
-    
+        
     /* On small screens, set height to 'auto' for sidenav and grid */
     @media screen and (max-width: 767px) {
       .sidenav {
@@ -37,12 +36,18 @@
      footer, .nv {
       background-color: #003B5C;
       color: white;
-      font-size: 100%;
+      font-size: 100%;  
+    }
+
+    footer{
+      height: 40%;
+      padding: 5px;
+      padding-bottom: 20px;
     }
 
     .logo-small {
-    color: #f4511e;
-    font-size: 30px;
+    color: #5d98f7;
+    font-size: 50px;
 }
 
 #cuadro{
@@ -57,7 +62,6 @@
 
  hr{border: 0; border-top: 1px solid #003B5C; border-bottom: 1px ;}
   
- 
   p{
     padding: 25px;
   }
@@ -65,13 +69,11 @@
   .icon-bar{ background-color: gray;}
 
   a.nv:hover{
-
   background-color: white;
   }
   
   .logo-small:hover{
-
-color: #ffbf80;
+color: #004bc4;
 }
 
 body{
@@ -80,8 +82,8 @@ body{
 
   </style>
 </head>
-<body>
 
+<body>
 <nav class="navbar nv">
   <div class="container">
     <div class="navbar-header">
@@ -93,81 +95,92 @@ body{
       <a class="navbar-brand nv" href="{{ url('/') }}">AccesBuap</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a class="nv" href="{{ url('/login') }}"><span class="glyphicon glyphicon-log-in nv"></span> Iniciar Sesión</a></li>
-      </ul>
+    <ul class="nav nav-pills navbar-right">
+    <!-- Authentication Links -->
+    @if (Auth::check())
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu" role="menu">
+                <li>
+                    <a 
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+            </ul>
+        </li>
+    @endif
+</ul>
     </div>
   </div>
 </nav>
   
 <div class="container-fluid text-center">    
     <div class="row content">
-      <div class="col-sm-12"> 
-        <div class="container-fluid text-center">
-          <h2 id="ser">SERVICIOS</h2>
-          <br>
-          <h4 id="ser">INICIA SESIÓN PARA REALIZAR ALGUNA DE LAS SIGUIENTES ACCIONES</h4>
-        <div class="row">
-  <hr> <!-- quitar lineas -->
-  <br>
-    <div class="col-sm-4" id="cuadro">
-      <span class="glyphicon glyphicon-duplicate logo-small"></span>
-      <h4>Registrar Accesos Comunidad Buap</h4>
+      <div class="col-sm-12">
+          <div class="container-fluid text-center">
+            <h2 id="ser">SERVICIOS</h2>
+            <br>
+            <h4 id="ser">INICIA SESIÓN PARA REALIZAR ALGUNA DE LAS SIGUIENTES ACCIONES</h4>
+           <div class="row">
+           <hr> <!-- quitar lineas -->
+      <br>
+      <br>
+      <br>
+      <br>
+       <div class="col-sm-3" id="cuadro">
+          <span class="glyphicon glyphicon-duplicate logo-small"></span>
+          <h4>Registrar Accesos Comunidad Buap</h4>
+       </div>
+       <div class="col-sm-2" id="cuadro">
+          <span class="glyphicon glyphicon-calendar logo-small"></span>
+         <h4>Registrar Salidas</h4>
+      </div>
+      <div class="col-sm-3" id="cuadro">
+         <span class="glyphicon glyphicon-user logo-small"></span>
+         <h4>Registrar Personas</h4>
+      </div>
+      <div class="col-sm-2" id="cuadro">
+        <span class="glyphicon glyphicon-road logo-small"></span>
+        <h4>Registrar Vehiculos</h4>
+      </div>
+      <div class="col-sm-2" id="cuadro" >
+        <span class="glyphicon glyphicon-bold logo-small"></span>
+        <h4>Registrar Bicicletas</h4>
+      </div>
+           </div>
+        </div>  
+      </div>
     </div>
-    <div class="col-sm-4" id="cuadro">
-      <span class="glyphicon glyphicon-user logo-small"></span>
-      <h4>Registrar Personas</h4>
-    </div>
-    <div class="col-sm-4" id="cuadro">
-      <span class="glyphicon glyphicon-road logo-small"></span>
-      <h4>Registrar Vehiculos</h4>
-    </div>
-    </div>
-    <br>
-  <div class="row">
-     <!-- hr quitar lineas -->
-  <br>
-    <div class="col-sm-4" id="cuadro" >
-      <span class="glyphicon glyphicon-bold logo-small"></span>
-      <h4>Registrar Bicicletas</h4>
-    </div>
-    <div class="col-sm-4" id="cuadro">
-      <span class="	glyphicon glyphicon-calendar logo-small"></span>
-      <h4>Regristrar Visita</h4>
-    </div>
-    <div class="col-sm-4" id="cuadro">
-      <span class="glyphicon glyphicon-search logo-small"></span>
-      <h4>Buscar Alumnos</h4>
-    </div>
-  </div>
-</div>
-    </div>
-  </div>
 </div>
 <hr>  <!-- quitar lineas -->
 <footer class="container-fluid text-center">
-<div class="row">
-    <div class="col-sm-2 col-md-4">
-    <div class="logo_buap">
-		<a target="blanks" href="http://www.buap.mx/">
-		<img src="http://radiobuap.com/wp-content/uploads/2014/07/EscudoBUAPnegro350x350.png"  width="180" height="185" background-color="white">
-		</a>
-	</div>
+    <div class="row">
+        <div class="col-sm-2 col-md-4">
+          <div class="logo_buap">
+		          <a target="blanks" href="http://www.buap.mx/">
+		          <img src="http://radiobuap.com/wp-content/uploads/2014/07/EscudoBUAPnegro350x350.png"  width="180" height="170" background-color="white">
+		          </a>
+	        </div>
+        </div>
+        <div class="col-sm-10 col-md-8" >
+           <p><strong>Benemérita Universidad Autónoma de Puebla</strong><br />
+            4 Sur 104 Centro Histórico C.P. 72000<br />
+            Teléfono +52 (222) 229 55 00</p>
+          <div class="redes">
+            <a href="https://twitter.com/buapoficial" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/twitter.png" /></a>
+            <a href="https://www.facebook.com/BUAPoficial" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/facebook.png" /></a>
+            <a href="https://instagram.com/buapoficial/" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/instagram.png" /></a>
+            <a href="https://www.youtube.com/user/ibuap" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/youtube.png" /></a>
+          </div>
+        </div>
     </div>
-    <div class="col-sm-10 col-md-8" >
-    <p><strong>Benemérita Universidad Autónoma de Puebla</strong><br />
-4 Sur 104 Centro Histórico C.P. 72000<br />
-Teléfono +52 (222) 229 55 00</p>
-    <div class="redes">
-<a href="https://twitter.com/buapoficial" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/twitter.png" /></a>
-<a href="https://www.facebook.com/BUAPoficial" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/facebook.png" /></a>
-<a href="https://instagram.com/buapoficial/" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/instagram.png" /></a>
-<a href="https://www.youtube.com/user/ibuap" target="_blank"><img src="http://www.buap.mx/sites/all/themes/nuevo_sitio/img/youtube.png" /></a>
-</div>
-    </div>
-  </div>
- 
 </footer>
-
 </body>
 </html>
